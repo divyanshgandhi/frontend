a60eaee8bacaf888eb1666242f624ce783fbb9b6
Divyansh Gandhi <dg1199@gmail.com>
2022-12-06 02:21:38 +0530

    React frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Background from "./components/background.component";
+import Weather from "./components/weather.component";
+import List from "./components/list.component";
+import Summary from "./components/summary.component";
+import { Footer } from "./components/footer.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Background >
+
+      <div className="navbar navbar-expand mb-4">
+        <a href="/" className=" text-white text-3xl italic no-underline font-bold ml-10 mt-2">
+          TwoDo
         </a>
-      </header>
-    </div>
+      </div>
+
+      <div className="relative md:flex lg:flex xl:flex h-4/5">
+        <div className="basis-1/2 p-10 ml-8 mt-8">
+          <Weather />
+          <br />
+          <Summary />
+        </div>
+        <div id="#slider" className="basis-1/2 h-80vh w-5/6 ml-8">
+          <List />
+        </div>
+      </div>
+
+      <Footer />
+
+    </Background>
   );
 }
 
